6315652bda13d502488b61859d19342c02ef7f00
Pavel Sobolev <paveloom@mail.ru>
2020-04-27 09:24:40 +0000

    Обновлена версия релиза.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 from Модуль.scats.build import get_pybind_include, BuildExt
 
-__version__ = '0.2.1-dev3'
+__version__ = '0.2.1'
 
 ext_modules = [
     Extension(
